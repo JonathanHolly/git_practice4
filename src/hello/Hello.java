fb16371ba2b78a90b43515d17d5976d782a8b995
jonat <jonathanhollywood@yahoo.com>
2022-01-24 07:39:34 -0800

    Doing final work on hello feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello wolrd");
+        System.out.println("B25 learning git and github");
 
     }
 }
